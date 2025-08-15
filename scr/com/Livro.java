1e95ce098866ef557c355a83ed0afc2cb44a8d60
RafaFeitos <133874840+RafaFeitos@users.noreply.github.com>
2025-08-15 04:47:25 -0300

    scr Livro.java

---
+++ scr/com/Livro.java
@@ -0,0 +1,50 @@
+package com.biblioteca;
+
+public class Livro implements ItemBiblioteca {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = true; // Livro inicialmente disponível
+    }
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public int getAno() {
+        return ano;
+    }
+
+    @Override
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    @Override
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Livro";
+    }
+
+    @Override
+    public String toString() {
+        return "Livro: " + titulo + ", Autor: " + autor + ", Ano: " + ano + ", Disponível: " + disponivel;
+    }
+}
